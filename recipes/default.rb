12ebc26bf4367a7ef4e72785160703d745c87461
Tim Smith <tsmith84@gmail.com>
2015-09-30 18:10:01 -0700

    Resolve foodcritic warnings

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: maradns
 # Recipe:: default
 #
-# Copyright 2009, Chef Software, Inc.
+# Copyright 2009-2015, Chef Software, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -43,8 +43,8 @@ template '/etc/maradns/mararc' do
 end
 
 # be sure to create the db.domain template.
-template "/etc/maradns/db.#{node[:domain]}" do
-  source "db.#{node[:domain]}.erb"
+template "/etc/maradns/db.#{node['domain']}" do
+  source "db.#{node['domain']}.erb"
   mode 0644
   owner 'root'
   group 'root'
